e577a9ece31042593d9321ff15ccf1dd44c72eeb
arata sekine <arata-sekine@aeonlink.co.jp>
2018-06-20 20:07:44 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports={
+    mode: "development",
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        filename: '[name].js',
+    },
+    //ビルド対象の拡張子設定
+    resolve: {
+        extensions: ['.js', '.json', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                resource: {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/
+                },
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ["env", "react"]
+                    }
+                }
+            }
+        ]
+    }
+};
